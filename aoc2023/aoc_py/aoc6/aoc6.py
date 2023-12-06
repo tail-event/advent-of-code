24205550530a48fbd7d72c3559b40dce81f1c1f8
tail-event <lorenzo.pratissoli@outlook.com>
2023-12-06 21:39:41 +0100

    aoc6 in python

---
+++ aoc2023/aoc_py/aoc6/aoc6.py
@@ -0,0 +1,47 @@
+import math
+import operator
+import os
+import time
+from functools import reduce
+
+
+def compute_n_holding_ways(time: int, distance: int) -> int:
+    """
+    Find winning holding milliseconds
+    by solving (time - x) * x > distance
+    where x is the holding time
+    """
+    b = -time
+    c = distance
+    a = 1
+
+    lower = (-b - math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+    upper = (-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+
+    lower_int = lower + 1 if int(lower) == lower else int(math.ceil(lower))
+    upper_int = upper - 1 if int(upper) == upper else int(math.floor(upper))
+    return int(upper_int - lower_int + 1)
+
+
+def first_part_solution(times: list[int], distances: list[int]) -> int:
+    possibilities = [compute_n_holding_ways(time, distance) for time, distance in zip(times, distances)]
+    return reduce(operator.mul, possibilities, 1)
+
+
+def second_part_solution(times: list[int], distances: list[int]) -> int:
+    time = int("".join(str(x) for x in times))
+    distance = int("".join(str(x) for x in distances))
+    return compute_n_holding_ways(time, distance)
+
+
+if __name__ == "__main__":
+    t0 = time.perf_counter()
+    with open(os.path.join(os.path.dirname(__file__), "in.txt"), mode="r", encoding="utf-8") as f:
+        times = [int(x) for x in f.readline().split(": ")[1].strip().split()]
+        distances = [int(x) for x in f.readline().split(": ")[1].strip().split()]
+
+        print(f"First part solution={first_part_solution(times, distances)}")
+        print(f"Took {time.perf_counter() - t0:.5f} seconds")
+
+        print(f"Second part solution={second_part_solution(times, distances)}")
+        print(f"Took {time.perf_counter() - t0:.5f} seconds")
